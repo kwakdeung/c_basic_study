fac2cfe37c886bc3a71f46b5d34f3aaf794ba283
kwakdeung <kjy@dncbiotech.com>
2025-09-17 23:38:07 +0900

    first commit

---
+++ 1_test.c
@@ -0,0 +1,21 @@
+// include: 가져오기 기능
+// <stdio.h>: 표준 입출력 라이브러리,
+// std - standard(표준)
+// i - input(표준 입력) => 키보드
+// o - output(표준 출력) => 모니터
+#include <stdio.h>
+
+// int main() - main 함수 생성
+// int - 함수의 return 타입
+// return 0 - 함수의 return value
+// ; - 문장의 마침표
+// {} - 중괄호, 블록(함수의 시작과 끝)
+int main(void)
+{
+    printf("안녕하세요\n");
+    printf("저는 \"홍길동\"입니다.\n"); // \" : 쌍따옴표 출력
+
+    printf("번호: %d, 나이: %d\n", 15, 20); // %d : decimal, 10진수 정수 출력, 서식지정자
+
+    return 0;
+}
