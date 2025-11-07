75d5b9ed151b293d632aa3ec7a72a72c8430a1a2
kwakdeung <rhkrwnsdud22@naver.com>
2025-11-07 17:23:45 +0900

    7_test.c

---
+++ 7_test.c
@@ -0,0 +1,51 @@
+// 10강 문제, 자료형 문제 풀이
+// // 문제1: 변수를 만들고 3.14를 넣은 후 출력해보세요.(가장 메모리를 효율적으로 사용해주세요.)
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   int i = 3.14; // 8 byte
+//   printf("i: %d\n", i);
+//
+//   float f = 3.14; // 4 byte
+//   printf("f: %d\n", f);
+//   printf("f: %f\n", f);
+//
+//   return 0;
+// }
+
+// // 문제2: 변수를 만들고 200을 넣은 후 출력해보세요.(가장 메모리를 효율적으로 사용해주세요.)
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   unsigned char i = 200; // 1 byte
+//   printf("i: %d\n", i);
+//
+//   return 0;
+// }
+
+// // 문제3: 변수를 만들고 22억을 넣은 후 출력해보세요.(가장 메모리를 효율적으로 사용해주세요.)
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   unsigned int a = 2200000000; // 4 byte
+//   printf("a: %u\n", a);
+//
+//   return 0;
+// }
+
+// // 문제4: 변수를 만들고 400억을 넣은 후 출력해보세요.(가장 메모리를 효율적으로 사용해주세요.)
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+//   long long a = 40000000000; // 8 byte
+//   printf("a: %lld\n", a);
+//
+//   return 0;
+// }
